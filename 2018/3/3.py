4aae9cc121a7565707444fdc16f22cff3b29ab6b
ezquire <tyler.gearing@gmail.com>
2019-09-03 19:06:17 -0700

    added second solution

--- 2018/3/3.py
+++ 2018/3/3.py
@@ -21,16 +21,24 @@ for rect in rects:
     y = int(rect[2])
     w = int(rect[3])
     h = int(rect[4])
-    valid_id = True
     for i in range(x, x + w):
         for j in range(y, y + h):
             if board[i][j] == 0:
                 board[i][j] = num
             elif board[i][j] > 0:
-                valid_id = False
                 board[i][j] = -1
                 total += 1
-            elif board[i][j] == -1:
+
+for rect in rects:
+    num = int(rect[0])
+    x = int(rect[1])
+    y = int(rect[2])
+    w = int(rect[3])
+    h = int(rect[4])
+    valid_id = True
+    for i in range(x, x + w):
+        for j in range(y, y + h):
+            if board[i][j] == -1:
                 valid_id = False
     if valid_id: print(num)
 
